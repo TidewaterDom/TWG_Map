2ba58cba70ce3ddb7de943e8084345c486cc76e3
TidewaterDom <58816628+TidewaterDom@users.noreply.github.com>
2019-12-13 17:56:31 +0000

    Delete bokehserverextension.py

--- bokehserverextension.py
+++
@@ -1,4 +0,0 @@
-from subprocess import Popen
-def load_jupyter_server_extension(nbapp):
-    """serve the bokeh_map directory with bokeh server"""
-    Popen(["bokeh", "serve", "bokeh_map", "--allow-websocket-origin=*"])
